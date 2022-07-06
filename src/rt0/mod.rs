d29f6b74495c5327479ddc4e950edf90cfb55088
ProfElements <profelements1@gmail.com>
2022-07-06 00:07:48 -0500

    :sparkles: feature(run): Now can get to main with everything setup

--- src/rt0/mod.rs
+++ src/rt0/mod.rs
@@ -3,3 +3,13 @@ core::arch::global_asm!{
     include_str!("init.S"), 
     options(raw) 
 }
+
+core::arch::global_asm!{
+    include_str!("hw_init.S"), 
+    options(raw) 
+}
+
+core::arch::global_asm!{
+    include_str!("cache.S"), 
+    options(raw) 
+}
