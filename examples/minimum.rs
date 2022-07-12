3a895955729fcb6ee2fe7ae1b37cc180b6c72dc6
ProfElements <profelements1@gmail.com>
2022-07-12 01:28:23 -0500

    :art: Cargo format file

--- examples/minimum.rs
+++ examples/minimum.rs
@@ -4,25 +4,29 @@
 
 extern crate alloc;
 
-use core::{panic::PanicInfo, alloc::Layout};
-use rosalina::{init::OS, print, allocator::init_heaps};
+use core::{alloc::Layout, panic::PanicInfo};
+
+use rosalina::{os::OS, print};
 
 #[no_mangle]
 extern "C" fn main() -> ! {
-    
     let _os = OS::init();
-        
+
     loop {}
 }
 
 #[panic_handler]
 fn panic_handler(info: &PanicInfo) -> ! {
-    print!("{}", info); 
+    print!("{}", info);
     loop {}
 }
 
 #[alloc_error_handler]
 fn alloc_handler(layout: Layout) -> ! {
-    print!("Failed to allocate item with \n Size: {}\n, Align: {}\n", layout.size(), layout.align()); 
+    print!(
+        "Failed to allocate item with \n Size: {}\n, Align: {}\n",
+        layout.size(),
+        layout.align()
+    );
     loop {}
 }
