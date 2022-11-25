28601cbf6cb5ff499081d32450e45ce689003a67
ProfElements <profelements1@gmail.com>
2022-11-25 05:57:40 -0600

    :bug: fix(MachineStateRegister): Used proper bits of MSR

--- src/arch.rs
+++ src/arch.rs
@@ -178,79 +178,79 @@ impl MachineStateRegister {
     }
 
     pub fn power_management(&self) -> PowerManagement {
-        self.0.get_bit(13).into()
+        self.0.get_bit(14).into()
     }
 
     pub fn with_power_management(&mut self, power: PowerManagement) -> &mut Self {
-        self.0.set_bit(13, power.into());
+        self.0.set_bit(14, power.into());
         self
     }
 
     pub fn exception_little_endian_mode(&self) -> Endianness {
-        self.0.get_bit(15).into()
+        self.0.get_bit(16).into()
     }
 
     pub fn with_exception_little_endian_mode(&mut self, endian: Endianness) -> &mut Self {
-        self.0.set_bit(15, endian.into());
+        self.0.set_bit(16, endian.into());
         self
     }
 
     pub fn external_interrupt_enabled(&self) -> Enabled {
-        self.0.get_bit(16).into()
+        self.0.get_bit(15).into()
     }
 
     pub fn with_external_interrupt_enabled(&mut self, enable: Enabled) -> &mut Self {
-        self.0.set_bit(16, enable.into());
+        self.0.set_bit(15, enable.into());
         self
     }
 
     pub fn priviledge(&self) -> Priviledge {
-        self.0.get_bit(17).into()
+        self.0.get_bit(14).into()
     }
 
     pub fn with_priviledge(&mut self, priviledge: Priviledge) -> &mut Self {
-        self.0.set_bit(17, priviledge.into());
+        self.0.set_bit(14, priviledge.into());
         self
     }
 
     pub fn floating_point_enable(&self) -> Enabled {
-        self.0.get_bit(18).into()
+        self.0.get_bit(13).into()
     }
 
     pub fn with_floating_point_enable(&mut self, enable: Enabled) -> &mut Self {
-        self.0.set_bit(18, enable.into());
+        self.0.set_bit(13, enable.into());
         self
     }
 
     pub fn machine_check_enable(&self) -> Enabled {
-        self.0.get_bit(19).into()
+        self.0.get_bit(12).into()
     }
 
     pub fn with_machine_check_enable(&mut self, enable: Enabled) -> &mut Self {
-        self.0.set_bit(19, enable.into());
+        self.0.set_bit(12, enable.into());
         self
     }
 
     pub fn step_trace_enable(&self) -> Enabled {
-        self.0.get_bit(21).into()
+        self.0.get_bit(10).into()
     }
 
     pub fn with_step_trace_enable(&mut self, enable: Enabled) -> &mut Self {
-        self.0.set_bit(21, enable.into());
+        self.0.set_bit(10, enable.into());
         self
     }
 
     pub fn branch_trace_enable(&self) -> Enabled {
-        self.0.get_bit(22).into()
+        self.0.get_bit(9).into()
     }
 
     pub fn with_branch_trace_enable(&mut self, enable: Enabled) -> &mut Self {
-        self.0.set_bit(22, enable.into());
+        self.0.set_bit(9, enable.into());
         self
     }
 
     pub fn floating_point_exception_mode(&self) -> FloatingPointExceptionMode {
-        match (self.0.get_bit(20), self.0.get_bit(23)) {
+        match (self.0.get_bit(11), self.0.get_bit(8)) {
             (false, false) => FloatingPointExceptionMode::Disabled,
             (false, true) => FloatingPointExceptionMode::Nonrecoverable,
             (true, false) => FloatingPointExceptionMode::Recoverable,
@@ -264,76 +264,76 @@ impl MachineStateRegister {
     ) -> &mut Self {
         match mode {
             FloatingPointExceptionMode::Disabled => {
-                self.0.set_bit(20, false);
-                self.0.set_bit(23, false);
+                self.0.set_bit(11, false);
+                self.0.set_bit(8, false);
             }
             FloatingPointExceptionMode::Nonrecoverable => {
-                self.0.set_bit(20, false);
-                self.0.set_bit(23, true);
+                self.0.set_bit(11, false);
+                self.0.set_bit(8, true);
             }
             FloatingPointExceptionMode::Recoverable => {
-                self.0.set_bit(20, true);
-                self.0.set_bit(23, false);
+                self.0.set_bit(11, true);
+                self.0.set_bit(8, false);
             }
             FloatingPointExceptionMode::Precise => {
-                self.0.set_bit(20, true);
-                self.0.set_bit(23, true);
+                self.0.set_bit(11, true);
+                self.0.set_bit(8, true);
             }
         }
         self
     }
 
     pub fn exception_prefix(&self) -> ExceptionPrefix {
-        self.0.get_bit(25).into()
+        self.0.get_bit(6).into()
     }
 
     pub fn with_exception_prefix(&mut self, prefix: ExceptionPrefix) -> &mut Self {
-        self.0.set_bit(25, prefix.into());
+        self.0.set_bit(6, prefix.into());
         self
     }
 
     pub fn instruction_translation_enable(&self) -> Enabled {
-        self.0.get_bit(26).into()
+        self.0.get_bit(5).into()
     }
 
     pub fn with_instruction_translation_enable(&mut self, enable: Enabled) -> &mut Self {
-        self.0.set_bit(26, enable.into());
+        self.0.set_bit(5, enable.into());
         self
     }
 
     pub fn data_translation_enable(&self) -> Enabled {
-        self.0.get_bit(27).into()
+        self.0.get_bit(4).into()
     }
 
     pub fn with_data_translation_enable(&mut self, enable: Enabled) -> &mut Self {
-        self.0.set_bit(27, enable.into());
+        self.0.set_bit(4, enable.into());
         self
     }
 
     pub fn performance_marked_mode(&self) -> Marked {
-        self.0.get_bit(29).into()
+        self.0.get_bit(2).into()
     }
 
     pub fn with_performance_marked_mode(&mut self, marked: Marked) -> &mut Self {
-        self.0.set_bit(29, marked.into());
+        self.0.set_bit(2, marked.into());
         self
     }
 
     pub fn external_exception_mode(&self) -> ExceptionMode {
-        self.0.get_bit(30).into()
+        self.0.get_bit(1).into()
     }
 
     pub fn with_external_exception_mode(&mut self, mode: ExceptionMode) -> &mut Self {
-        self.0.set_bit(30, mode.into());
+        self.0.set_bit(1, mode.into());
         self
     }
 
     pub fn little_endian_mode(&self) -> Endianness {
-        self.0.get_bit(31).into()
+        self.0.get_bit(0).into()
     }
 
     pub fn with_little_endian_mode(&mut self, endian: Endianness) -> &mut Self {
-        self.0.set_bit(31, endian.into());
+        self.0.set_bit(0, endian.into());
         self
     }
 }
