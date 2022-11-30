0b69eac798473675bd351cea95a0334c19ba89d1
ProfElements <profelements1@gmail.com>
2022-11-30 04:22:22 -0600

    :recycle: refactor(clock): Constify clock, add `set_time` func to setup ticks to match rtc

--- src/clock.rs
+++ src/clock.rs
@@ -31,18 +31,35 @@ impl Instant {
     }
 
     pub fn secs(&self) -> u64 {
-        self.ticks / ((243000000 / 4000) * 1000)
+        self.ticks / (TB_TIMER_CLOCK * 1000)
     }
 
     pub fn millisecs(&self) -> u64 {
-        self.ticks / (243000000 / 4000)
+        self.ticks / TB_TIMER_CLOCK
     }
 
     pub fn microsecs(&self) -> u64 {
-        (self.ticks * 8) / ((243000000 / 4000) / 125)
+        (self.ticks * 8) / (TB_TIMER_CLOCK / 125)
     }
 
     pub fn nanosecs(&self) -> u64 {
-        (self.ticks * 8000) / ((243000000 / 4000) / 125)
+        (self.ticks * 8000) / (TB_TIMER_CLOCK / 125)
     }
 }
+
+pub fn set_time(time: u64) {
+    let time_upper: u32 = time.get_bits(32..=63).try_into().unwrap();
+    let time_lower: u32 = time.get_bits(0..=31).try_into().unwrap();
+    unsafe {
+        core::arch::asm!(
+            "mttbu {time_upper}",
+            "mttbl {time_lower}",
+            time_upper = in(reg) time_upper,
+            time_lower = in(reg) time_lower,
+        );
+    }
+}
+
+pub const TB_CORE_CLOCK: u64 = 729000000;
+pub const TB_BUS_CLOCK: u64 = 243000000;
+pub const TB_TIMER_CLOCK: u64 = TB_BUS_CLOCK / 4000;
