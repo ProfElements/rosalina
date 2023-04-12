392052dc09d9f154170c36517f7535f24a7e2f81
ProfElements <profelements1@gmail.com>
2023-04-12 01:32:02 -0500

    :sparkles: feat(ios): Added `metadata()` you can now get the size and seek position of files!

--- src/ios.rs
+++ src/ios.rs
@@ -1,4 +1,4 @@
-use self::internal::{Error, IosFileDesc};
+use self::internal::IosFileDesc;
 
 pub enum FileAccessMode {
     None,
@@ -22,9 +22,45 @@ pub struct Ios {
     fd: IosFileDesc,
 }
 
+#[repr(C)]
 #[derive(Debug)]
-pub enum FileError {
-    NoSuchFile,
+pub enum Error {
+    PermissionDenied,
+    FileExists,
+    InvalidArg,
+    FileNotFound,
+    ResourceBusy,
+    Ecc,
+    AllocFailed,
+    CorruptedFile,
+    TooManyFiles,
+    PathToLong,
+    FileisAlreadyOpen,
+    DirectoryNotEmpty,
+    Fatal,
+}
+#[repr(C)]
+#[derive(Debug)]
+pub struct Metadata {
+    len: u32,
+    seek_pos: u32,
+}
+
+impl Metadata {
+    pub const fn new() -> Self {
+        Self {
+            len: 0,
+            seek_pos: 0,
+        }
+    }
+
+    pub fn len(&self) -> usize {
+        usize::try_from(self.len).unwrap()
+    }
+
+    pub fn is_empty(&self) -> bool {
+        self.len() == 0
+    }
 }
 
 impl Ios {
@@ -43,15 +79,24 @@ impl Ios {
     pub fn fd(&self) -> usize {
         self.fd.0.fd.try_into().unwrap()
     }
+    /// # Errors
+    /// Any related IOS errors see `Error`
+    pub fn metadata(&self) -> Result<Metadata, Error> {
+        let mut metadata = Metadata::new();
+        self.fd.ioctl(11, &[], &mut metadata)?;
+        crate::println!("{:?}", metadata);
+
+        Ok(metadata)
+    }
 }
 
 mod internal {
 
     use alloc::ffi::CString;
 
-    use crate::ipc::{ios_close, ios_open, ios_read};
+    use crate::ipc::{ios_close, ios_ioctl, ios_open, ios_read};
 
-    use super::FileAccessMode;
+    use super::{Error, FileAccessMode};
 
     type IosRawFd = isize;
 
@@ -67,23 +112,6 @@ mod internal {
 
     pub struct IosFileDesc(pub(crate) OwnedIosFd);
 
-    #[repr(C)]
-    #[derive(Debug)]
-    pub enum Error {
-        PermissionDenied,
-        FileExists,
-        InvalidArg,
-        FileNotFound,
-        ResourceBusy,
-        Ecc,
-        AllocFailed,
-        CorruptedFile,
-        TooManyFiles,
-        PathToLong,
-        FileisAlreadyOpen,
-        DirectoryNotEmpty,
-        Fatal,
-    }
     /*
     pub enum FileSeek {
         Start(usize),
@@ -132,46 +160,46 @@ mod internal {
             }
         }
         /*
-        pub fn write(&self, buf: &[u8]) -> Result<usize, Error> {
-            match ios_write(self.0.fd, buf) {
-                -1 | -102 => Err(Error::PermissionDenied),
-                -2 | -105 => Err(Error::FileExists),
-                -4 | -101 => Err(Error::InvalidArg),
-                -6 | -106 => Err(Error::FileNotFound),
-                -8 | -118 => Err(Error::ResourceBusy),
-                -12 | -114 => Err(Error::Ecc),
-                -103 => Err(Error::CorruptedFile),
-                -107 | -109 => Err(Error::TooManyFiles),
-                -22 | -108 => Err(Error::AllocFailed),
-                -110 | -116 => Err(Error::PathToLong),
-                -111 => Err(Error::FileisAlreadyOpen),
-                -115 => Err(Error::DirectoryNotEmpty),
-                -119 => Err(Error::Fatal),
-                val => Ok(val.try_into().unwrap()),
-            }
-        }
+                pub fn write(&self, buf: &[u8]) -> Result<usize, Error> {
+                    match ios_write(self.0.fd, buf) {
+                        -1 | -102 => Err(Error::PermissionDenied),
+                        -2 | -105 => Err(Error::FileExists),
+                        -4 | -101 => Err(Error::InvalidArg),
+                        -6 | -106 => Err(Error::FileNotFound),
+                        -8 | -118 => Err(Error::ResourceBusy),
+                        -12 | -114 => Err(Error::Ecc),
+                        -103 => Err(Error::CorruptedFile),
+                        -107 | -109 => Err(Error::TooManyFiles),
+                        -22 | -108 => Err(Error::AllocFailed),
+                        -110 | -116 => Err(Error::PathToLong),
+                        -111 => Err(Error::FileisAlreadyOpen),
+                        -115 => Err(Error::DirectoryNotEmpty),
+                        -119 => Err(Error::Fatal),
+                        val => Ok(val.try_into().unwrap()),
+                    }
+                }
 
-        pub fn seek(&self, pos: FileSeek) -> Result<usize, Error> {
-            match ios_seek(self.0.fd, pos.into()) {
-                -1 | -102 => Err(Error::PermissionDenied),
-                -2 | -105 => Err(Error::FileExists),
-                -4 | -101 => Err(Error::InvalidArg),
-                -6 | -106 => Err(Error::FileNotFound),
-                -8 | -118 => Err(Error::ResourceBusy),
-                -12 | -114 => Err(Error::Ecc),
-                -103 => Err(Error::CorruptedFile),
-                -107 | -109 => Err(Error::TooManyFiles),
-                -22 | -108 => Err(Error::AllocFailed),
-                -110 | -116 => Err(Error::PathToLong),
-                -111 => Err(Error::FileisAlreadyOpen),
-                -115 => Err(Error::DirectoryNotEmpty),
-                -119 => Err(Error::Fatal),
-                val => Ok(val.try_into().unwrap()),
-            }
-        }
-
-        pub fn ioctl(&self, ioctl: usize, buf_in: &[u8], buf_out: &[u8]) -> Result<(), Error> {
-            match ios_ioctl(ioctl, buf_in, buf_out) {
+                pub fn seek(&self, pos: FileSeek) -> Result<usize, Error> {
+                    match ios_seek(self.0.fd, pos.into()) {
+                        -1 | -102 => Err(Error::PermissionDenied),
+                        -2 | -105 => Err(Error::FileExists),
+                        -4 | -101 => Err(Error::InvalidArg),
+                        -6 | -106 => Err(Error::FileNotFound),
+                        -8 | -118 => Err(Error::ResourceBusy),
+                        -12 | -114 => Err(Error::Ecc),
+                        -103 => Err(Error::CorruptedFile),
+                        -107 | -109 => Err(Error::TooManyFiles),
+                        -22 | -108 => Err(Error::AllocFailed),
+                        -110 | -116 => Err(Error::PathToLong),
+                        -111 => Err(Error::FileisAlreadyOpen),
+                        -115 => Err(Error::DirectoryNotEmpty),
+                        -119 => Err(Error::Fatal),
+                        val => Ok(val.try_into().unwrap()),
+                    }
+                }
+        */
+        pub fn ioctl<T>(&self, ioctl: usize, buf_in: &[u8], buf_out: &mut T) -> Result<(), Error> {
+            match ios_ioctl(self.0.fd, ioctl, buf_in, buf_out) {
                 -1 | -102 => Err(Error::PermissionDenied),
                 -2 | -105 => Err(Error::FileExists),
                 -4 | -101 => Err(Error::InvalidArg),
@@ -188,7 +216,7 @@ mod internal {
                 _ => Ok(()),
             }
         }
-
+        /*
         pub fn ioctlv(
             &self,
             ioctl: usize,
