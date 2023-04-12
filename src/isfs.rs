5abdaa5db422719e06c514f51ee7d445d7713a96
ProfElements <profelements1@gmail.com>
2023-04-12 01:37:53 -0500

    :construction: feat(isfs): Add some isfs stuff, making it easier to read files and handle some data

---
+++ src/isfs.rs
@@ -0,0 +1,20 @@
+use alloc::vec;
+use alloc::vec::Vec;
+
+use crate::ios::{Error, FileAccessMode, Ios};
+
+/// # Errors
+/// any related Ios errors see `ios::Error`
+pub fn read(path: impl AsRef<str>) -> Result<Vec<u8>, Error> {
+    let mut file = open(path.as_ref())?;
+    let size = file.metadata().map(|m| m.len()).unwrap_or(0);
+    let mut bytes = vec![0u8; size];
+    file.read(&mut bytes);
+    Ok(bytes)
+}
+/// # Errors
+/// any related Ios errors see `ios::Error`
+pub fn open(path: impl AsRef<str>) -> Result<Ios, Error> {
+    let file = Ios::open(path.as_ref(), FileAccessMode::Read)?;
+    Ok(file)
+}
